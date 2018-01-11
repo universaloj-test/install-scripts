0f3b15a3c1920eab80df28ad5e612255a3fbe453
billchenchina <billchenchina2001@gmail.com>
2018-01-11 10:42:48 +0800

    update

--- install.sh
+++ install.sh
@@ -78,18 +78,9 @@ echo "Download UOJ Source Code Finished"
 ################## Download UOJ Source Code Finished ##################
 ################## Configure UOJ Start ##################
 echo "Configure UOJ Start"
-cp uoj-web /root/uoj_1 -R
-cp uoj-judge /root/judge_client_1 -R
-cp ~/install-scripts/install/new_problem.sh \
-	~/install-scripts/install/post-commit.sh \
-	~/install-scripts/install/uoj-passwd \
-	~/install-scripts/install/uoj-post-commit \
-	~/install-scripts/install/gen-uoj-config.php \
-	~/install-scripts/install/app_uoj233.sql \
-	~/install-scripts/install/install \
-	~/install-scripts/install/up \
-	/root/
-cd ~ && php gen-uoj-config.php && chmod +x install && ./install && rm * -rf
+cp uoj-web/* /root/uoj_1 -R
+cp uoj-judge/* /root/judge_client_1 -R
+cd ~/install-scripts/install/ && php gen-uoj-config.php && chmod +x install && ./install && rm * -rf
 cd ~ &&wget https://raw.githubusercontent.com/universaloj-test/install-scripts/master/install/up && chmod +x /root/up
 cd ~/ && rm -rf install-scripts
 echo "Configure UOJ Finished"
